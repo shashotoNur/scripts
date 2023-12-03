b225410b7505669b6399a8a9d314f3319d1bd238
shashoto-nur <shashoto.nur@proton.me>
2023-12-03 22:16:15 +0600

    modded

--- isitup.sh
+++ isitup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-WEBSITE="example.com"
-if curl -s --head $WEBSITE | grep "200 OK" > /dev/null; then
+
+# Check if a website is up or down
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <website>"
+    exit 1
+fi
+
+WEBSITE="$1"
+
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$WEBSITE")
+
+if [ "$STATUS_CODE" == "200" ]; then
     echo "Website is up!"
 else
-    echo "Website is down!"
+    echo "Website is down! (Status Code: $STATUS_CODE)"
 fi
--- pwgen.sh
+++ pwgen.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
-LENGTH=12
-tr -dc A-Za-z0-9 < /dev/urandom | head -c $LENGTH | xargs
+
+MIN_LENGTH=15
+MAX_LENGTH=20
+REQUIRED_UPPER=3
+REQUIRED_LOWER=3
+REQUIRED_SYMBOLS=3
+REQUIRED_NUMBERS=3
+
+SYMBOLS="!@+#$% ^&*(_)-=<>?"
+
+LENGTH=$(shuf -i "$MIN_LENGTH"-"$MAX_LENGTH" -n 1)
+
+# Generate required components
+upper=$(tr -dc 'A-Z' < /dev/urandom | head -c "$REQUIRED_UPPER")
+lower=$(tr -dc 'a-z' < /dev/urandom | head -c "$REQUIRED_LOWER")
+symbols=$(tr -dc "$SYMBOLS" < /dev/urandom | head -c "$REQUIRED_SYMBOLS")
+numbers=$(tr -dc '0-9' < /dev/urandom | head -c "$REQUIRED_NUMBERS")
+
+# Calculate the length of the remaining characters
+rest_length=$((LENGTH - REQUIRED_UPPER - REQUIRED_LOWER - REQUIRED_SYMBOLS - REQUIRED_NUMBERS))
+
+# Generate the remaining characters
+rest=$(tr -dc "A-Za-z0-9$SYMBOLS" < /dev/urandom | head -c "$rest_length")
+
+# Combine and shuffle all components
+PASSWORD=$(echo "$upper$lower$symbols$numbers$rest" | fold -w1 | shuf | tr -d '\n')
+
+# Copy password to clipboard (requires xclip or xsel)
+echo -n "$PASSWORD" | xclip -selection clipboard
+echo "Password generated and copied to clipboard!"
