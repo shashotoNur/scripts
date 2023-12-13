9ef373459f18c89348e97d1a9bee69139d2ca083
shashoto-nur <shashoto.nur@proton.me>
2023-12-13 10:20:46 +0600

    better readability

--- disperse_contents.sh
+++ disperse_contents.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 
 # Get the variables from user input if they aren't provided as arguments
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo -e "Usage: $0 /path/to/source <target_size>\n"
+
     read -p "Enter the path to the source directory: " source_directory
-    read -p "Enter the path to the target directory: " target_directory
     read -p "Enter the target subdirectory size in megabytes: " target_size_mb
 else
     source_directory="$1"
-    target_directory="$2"
-    target_size_mb="$3"
+    target_size_mb="$2"
 fi
 
-# Convert megabytes to bytes
+# Convert megabytes to kilobytes
 target_size=$((target_size_mb * 1024))
-mkdir -p "$target_directory"
 
-# Create all subdirectories in advance
-num_subdirs=$(( ($(du -s "$source_directory" | cut -f1) + target_size - 1) / target_size ))
+# Get the size of the source directory in kilobytes
+source_size=$(du -s "$source_directory" | cut -f1)
+
+# Calculate the number of required subdirectories
+num_subdirs=$((source_size / target_size))
+
+# Create the required subdirectories
 for ((i = 0; i < num_subdirs; i++)); do
-    mkdir -p "${target_directory}/subdir_${i}"
+    mkdir -p "${source_directory}/${i}"
 done
 
 # Create an array of filenames sorted by size
-readarray -t sorted_files < <(find "$source_directory" -type f -printf "%s %p\n" | sort -n | cut -d' ' -f2-)
+readarray -t sorted_files < <(
+    find "$source_directory" -type f -printf "%s %p\n" |  # Print size and path
+    sort -n |  # Sort by size
+    cut -d' ' -f2-  # Cut to get only the path
+)
 
-# Copy files in a round-robin manner to subdirectories
+# Move files in a round-robin manner to subdirectories
 sub_dir_index=0
 for file in "${sorted_files[@]}"; do
-    sub_dir="${target_directory}/subdir_${sub_dir_index}"
-    cp "$file" "$sub_dir"
+    sub_dir="${source_directory}/${sub_dir_index}"
+    mv "$file" "$sub_dir"
+
     ((sub_dir_index = (sub_dir_index + 1) % num_subdirs))
 done
 
 # Display a message indicating completion
-echo "Directory divided into subdirectories in: $target_directory"
+echo "$source_directory contents are now divided into its subdirectories."
