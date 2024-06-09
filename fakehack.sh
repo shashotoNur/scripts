365c86d198a2d5802888648070c6c7c8b9c5bb36
Shashoto Nur <shashoto.nur@proton.me>
2024-06-09 11:44:37 +0600

    fakehack

--- fakehack.sh
+++ fakehack.sh
@@ -2,19 +2,34 @@
 echo -en "\e[32m"
 for((i=0;i<101;i++))
 do
-	echo -en "$(clear)Accessing the DataBase $i% complete...\n[$(for((j=i;j;j--));do echo -n █;done;for((j=100-i;j;j--));do echo -n ' ';done;echo ];sleep 0.1)\n"
+	echo -en "$(clear)Accessing root privileges: $i%\n[$(for((j=i;j;j--));do echo -n █;done;for((j=100-i;j;j--));do echo -n ' ';done;echo ];sleep 0.05)\n"
 done
-echo "Accessed the DataBase successfully!"
-sleep 2
+echo "Complete system access granted!"
+
+sleep 1
+
 for((i=0;i<101;i++))
 do
-	echo -en "$(clear)Accessing the DataBase 100% complete...\n[████████████████████████████████████████████████████████████████████████████████████████████████████]\nAccessed the DataBase successfully!\n\nCracking the code $i% complete...\n[$(for((j=i;j;j--));do echo -n █;done;for((j=100-i;j;j--));do echo -n ' ';done;echo ];sleep 0.1)"
+	echo -en "$(clear)Accessing root privileges: 100%\n[████████████████████████████████████████████████████████████████████████████████████████████████████]\nComplete system access granted!!\n\nTransferring control to remote admin: $i%\n[$(for((j=i;j;j--));do echo -n █;done;for((j=100-i;j;j--));do echo -n ' ';done;echo ];sleep 0.05)"
 done
-echo -e "\nCracked the code successfully!\n"
+echo -e "\nStable connection established...\n"
+
 sleep 0.5
-echo Granting access...
-sleep 2
-echo "Access fully granted!"
-sleep 2
-echo -e "\n$(case $1 in '')echo The device;;*)echo $1;;esac) has been hacked successfully!"
-xterm -T "Receiving Data" -n "Receiving Data" -e "echo -en \"\e[0;32m\";while true;do shuf -zi 0-1 -n 1;done"
+
+echo Remote is accessing user data...
+
+sleep 1
+
+echo -e "Access fully granted!\n"
+
+sleep 1
+
+echo -e "Device $(hostname)'s boot firmware corruption complete."
+
+env -u SESSION_MANAGER xterm -T "Transferring Data" -n "Transferring Data" -e "echo -en \"\e[0;32m\";while true;do shuf -zi 0-1 -n 1;done"
+
+echo -e "Desired target reached!!!\n"
+
+sleep 1
+
+echo -e "Warning: System status CRITICAL!\nChances of recovery < 1.2%"
